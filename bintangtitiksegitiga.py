aaf6fc4c5063e4df101ed965e6b92d15fb51fd61
LaluNabiel26 <lalun2612@gmail.com>
2024-10-19 15:54:40 +0800

    bintangtitiksegitiga.py

--- bintangtitiksegitiga.py
+++ bintangtitiksegitiga.py
@@ -1,12 +1,3 @@
-def print_star_pattern(n):
-    for i in range(1, n + 1):
-        spaces = ' ' * (n - i)
-        print(spaces, end='')  
-        for j in range(1, i + 1):
-            if j == 1:
-                print('*', end='') 
-            else:
-                print('.*', end='')  
-        print()  
 n = 5
-print_star_pattern(n)
+for i in range(n):
+    print(" " * (n - i - 1) + "*." * i + "*")
